4682fb1528880dfe7604fcc4c692a78ae4b2659c
Gonzalo Arechavala <gonzaloap@hotmail.com>
2021-11-21 14:29:57 +0100

    Version con pasador automatico de cartas

--- js/pokedex_auto.js
+++ js/pokedex_auto.js
@@ -6,9 +6,11 @@ Pagina web Pokedex: Gonzalo Arechavala
 Puedes ver el proyecto online en: 
 https://www.arechavala.es/upgrade-hup/pokedex/
 
+
+Vesión con pasador automático de cartas.
+-----------------------------------------
+
 */
-
-
 
 
 // Variable donde cargamos el elemento del HTML para cargar despues los datos.
@@ -18,7 +20,36 @@ const pokedex = document.getElementById('pokedex');
 // Variables para los botones HTML.
 const btnAnterior = document.querySelector(".btn-anterior");
 const btnSiguiente = document.querySelector(".btn-siguiente");
+const btnCuentaAtras = document.querySelector(".btn-cuenta-atras");
+const divCuentaAtras = document.querySelector(".cuenta-atras");
 
+
+// Variables para la cuenta atrás.
+let iniciamosTiempo;
+let numeroCuentaAtras;
+
+const pasaPagina = () => {
+    iniciamosTiempo = setInterval(cuentaAtras, 500);
+    numeroCuentaAtras = 10;
+}
+
+const pintarCuentaAtras = () => {
+    divCuentaAtras.innerHTML = numeroCuentaAtras;
+}
+
+const cuentaAtras = () => {
+
+    if (numeroCuentaAtras > 0) {
+        pintarCuentaAtras();
+
+        numeroCuentaAtras -= 1;
+    } else {
+        pintarCuentaAtras(); // para que salga el cero.
+        numeroCuentaAtras = 10;
+        pagSiguiente();
+    }
+
+}
 
 
 // Iniciamos los valores de la paginación, de 24 en 24, y ocultanto el primer boton anterior.
@@ -27,34 +58,107 @@ let cardFinal = 24;
 btnAnterior.style.display = 'none';
 
 btnSiguiente.addEventListener("click", () => {
+    // Paramos cuenta atra por si está activa y ponemos play.
+    btnCuentaAtras.innerHTML = 'Play';
+    clearInterval(iniciamosTiempo);
+    divCuentaAtras.innerHTML = ' ';
+
     // console.log("diste click siguiente");
-    if (cardInicial > 0){
-        cardInicial += 24;
-        cardFinal += 24;
-        btnAnterior.style.display = 'block';
-    }
-    if(cardFinal > 864){
-        btnSiguiente.style.display = 'none';
-        btnAnterior.style.display = 'block';
-    }
-    pokedexApi();
+    pagSiguiente();
+
 });
 
 btnAnterior.addEventListener("click", () => {
+    // Paramos cuenta atra por si está activa y ponemos play.
+    btnCuentaAtras.innerHTML = 'Play';
+    clearInterval(iniciamosTiempo);
+    divCuentaAtras.innerHTML = ' ';
+
     //console.log("diste click anterior");
-    if (cardInicial > 24){
-        cardInicial -= 24;
-        cardFinal -= 24;
-        btnSiguiente.style.display = 'block';
+    pagAnterior();
+});
+
+btnCuentaAtras.addEventListener("click", () => {
+    // if (btnCuentaAtras.textContent == 'Play') {
+    //     //console.log("diste click pasapagina y no es final");
+    //     btnCuentaAtras.textContent = 'Stop';
+    //     pasaPagina();
+    // } else if (btnCuentaAtras.textContent == 'Stop') {
+    //     //console.log("diste click pasapagina y no es final");
+    //     btnCuentaAtras.textContent = 'Play';
+    //     clearInterval(iniciamosTiempo);
+    //     divCuentaAtras.innerHTML = ' ';
+    // }
+
+    switch (btnCuentaAtras.textContent) {
+        case 'Play':
+            btnCuentaAtras.textContent = 'Stop';
+            pasaPagina();
+            break;
+
+        case 'Stop':
+            btnCuentaAtras.textContent = 'Play';
+            clearInterval(iniciamosTiempo);
+            divCuentaAtras.innerHTML = ' ';
+            break;
+
+        default: // Para 'Reiniciar'o cuanquier otro caso.
+            divCuentaAtras.innerHTML = ' ';
+            btnCuentaAtras.textContent = 'Play';
+            btnSiguiente.style.display = 'block';
+            btnAnterior.style.display = 'none';
+            clearInterval(iniciamosTiempo);
+            cardInicial = 1;
+            cardFinal = 24;
+            pokedexApi();
+            break;
     }
-    if (cardInicial < 24){
-        btnSiguiente.style.display = 'block';
-        btnAnterior.style.display = 'none';
-    }
-    pokedexApi();
 });
 
 
+
+
+// Aqui las funciones de paginación, para ser llamadas desde otros sitios.
+const pagSiguiente = () => {
+    if (cardInicial > 0) {
+        cardInicial += 24;
+        cardFinal += 24;
+        btnAnterior.style.display = 'block';
+        divCuentaAtras.innerHTML = ' ';
+        numeroCuentaAtras = 10;
+    }
+    if (cardFinal > 864) {
+        btnSiguiente.style.display = 'none';
+        btnAnterior.style.display = 'block';
+        // Paramos el pasador automatico y ponemos final.
+        btnCuentaAtras.innerHTML = 'Reiniciar';
+        clearInterval(iniciamosTiempo);
+        divCuentaAtras.innerHTML = 'Has llegado al final de los Pokémon';
+    }
+    pokedexApi();
+}
+
+const pagAnterior = () => {
+    if (cardInicial > 24) {
+        cardInicial -= 24;
+        cardFinal -= 24;
+        btnSiguiente.style.display = 'block';
+        divCuentaAtras.innerHTML = ' ';
+        numeroCuentaAtras = 10;
+        // Controlamos el texto del boton.
+        if (btnCuentaAtras.textContent == 'Reiniciar') {
+            btnCuentaAtras.textContent = 'Play';
+        }
+    }
+    if (cardInicial < 24) {
+        btnSiguiente.style.display = 'block';
+        btnAnterior.style.display = 'none';
+        divCuentaAtras.innerHTML = 'Has llegado al inicio de los Pokémon';
+    }
+    // Paramos contador y boton a play.
+
+    pokedexApi();
+}
 
 
 
@@ -118,7 +222,7 @@ const cambioTipo = (type) => {
             break;
 
         default:
-            return ('XXX ' + type + ' XXX');
+            return (type);
             break;
     }
 }
